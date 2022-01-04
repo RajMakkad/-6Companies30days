49fa240afd5a8d575164abe42ff2f8b687a16427
RajMakkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-05 01:13:56 +0530

    5th Problem

---
+++ Company Name -  Goldman Sachs/Ugly Numbers.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ll long long int
+#define ull unsigned long long int
+
+// https://practice.geeksforgeeks.org/problems/ugly-numbers2254/1/
+
+class Solution{
+public:	
+	ull getNthUglyNo(int n) {
+	    ull i(1),i2(0),i3(0),i5(0);
+
+		vector<int> ugly(n);
+		ugly[0] = 1;
+
+		ull nm2(ugly[i2]),nm3(ugly[i3]),nm5(ugly[i5]);
+
+		for(int i = 1;i<n;i++){
+			ull nexti = min({nm2,nm3,nm5});
+			ugly[i] = nexti;
+
+			if(nexti == nm2){
+				i2++;
+				nm2 = ugly[i2];
+			}
+			if(nexti == nm3){
+				i3++;
+				nm3 = ugly[i3];
+			}
+			if(nexti == nm5){
+				i5++;
+				nm5 = ugly[i5];
+			}
+		}
+
+		return ugly[n - 1];
+	}
+};
