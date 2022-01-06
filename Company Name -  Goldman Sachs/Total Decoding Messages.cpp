358264c7463797e5b7ca8d6cb4b45d5b6698f2e0
Raj Makkad <74362791+RajMakkad@users.noreply.github.com>
2022-01-06 17:10:45 +0530

    8th Problem

--- Company Name -  Goldman Sachs/Total Decoding Messages.cpp
+++ Company Name -  Goldman Sachs/Total Decoding Messages.cpp
@@ -11,19 +11,19 @@ using namespace __gnu_pbds;
 class Solution {
 	public:
 		int CountWays(string str){
-            int n = str.length();
-		    int dp[n + 1] = {0};
+		    int n = str.length();
+			    int dp[n + 1] = {0};
 
-            dp[0] = 1;
+		    dp[0] = 1;
 
-            for(int i = 1;i<=n;i++){
-                if(str[i - 1] != '0') dp[i] = dp[i - 1];
-                if(i>1 and str[i - 2] != '0'){
-                    ll num = (str[i - 2] - '0') * 10 + (str[i - 1] - '0');
-                    if(num>=1 and num<=26) dp[i] += dp[i - 2];
-                }
-            }
+		    for(int i = 1;i<=n;i++){
+			if(str[i - 1] != '0') dp[i] = dp[i - 1];
+			if(i>1 and str[i - 2] != '0'){
+			    ll num = (str[i - 2] - '0') * 10 + (str[i - 1] - '0');
+			    if(num>=1 and num<=26) dp[i] += dp[i - 2];
+			}
+		    }
 
-            return dp[n];
+		    return dp[n];
 		}
 };
